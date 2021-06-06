f0e5e4c5213cb3a4ae3912fb6ec92bb432f2ec80
josh sorensen <jxsoren@gmail.com>
2021-06-05 18:21:19 -0600

    deployment

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ require('dotenv').config()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const expressJwt = require('express-jwt')
+require("dotenv").config()
+
+const path = require("path")
+const secret = process.env.SECRET || "hello man cryo frozen"
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+const port = process.env.PORT || 8000;
 
 mongoose.connect(
     'mongodb://localhost:27017/rock_the_vote',
@@ -33,6 +41,11 @@ app.use((err, req, res, next) => {
     return res.send({ errMsg: err.message })
 })
 
-app.listen(9000, () => {
-    console.log('Server is running on local port 9000')
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on local port 9000 ${port}`)
 })
