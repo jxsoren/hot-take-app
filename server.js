d88ebe2776cab4b165a55d0d7d3d51a898c74314
josh sorensen <jxsoren@gmail.com>
2021-06-07 21:09:35 -0600

    mmongo_uri cinnection things

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express')
 const app = express()
-require('dotenv').config()
+const dotenv = require('dotenv')
+dotenv.config()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const expressJwt = require('express-jwt')
-require("dotenv").config()
 
 const path = require("path")
 const secret = process.env.SECRET || "hello man cryo frozen"
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
-
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, "client", "build")))
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', MONGO_URI => console.log('Connected to Database'))
+
 
 const port = process.env.PORT || 8000;
 
