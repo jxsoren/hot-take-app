daf574f60c19dda19299e1683e1721f7309604d2
josh sorensen <jxsoren@gmail.com>
2021-06-08 15:56:01 -0600

    express mid

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.json())
 app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, "client", "build")))
 
+
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -26,39 +27,38 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', MONGO_URI => console.log('Connected to Database'))
 
-
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
 // mongoose.connect(
-//     'mongodb://localhost:27017/rock_the_vote',
-//     {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true,
-//         useCreateIndex: true,
-//         useFindAndModify: false
-//     },
-//     () => console.log('Connected to the Database')
-// )
-
-app.use('/auth', require('./routes/authRouter.js'))
-
-// gatekeeper 
-app.use('/api', expressJwt({ secret: process.env.SECRET, algorithms:['HS256'] }))
-app.use('/api/issues', require('./routes/issueRouter.js'))
-
-app.use((err, req, res, next) => {
-    console.log(err)
-    if(err.name === "UnauthorizedError"){
-        res.status(err.status)
-    }
-    return res.send({ errMsg: err.message })
-})
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
-
-
-app.listen(port, () => {
+    //     'mongodb://localhost:27017/rock_the_vote',
+    //     {
+        //         useNewUrlParser: true,
+        //         useUnifiedTopology: true,
+        //         useCreateIndex: true,
+        //         useFindAndModify: false
+        //     },
+        //     () => console.log('Connected to the Database')
+        // )
+        
+        app.use('/auth', require('./routes/authRouter.js'))
+        
+        // gatekeeper 
+        app.use('/api', expressJwt({ secret: process.env.SECRET, algorithms:['HS256'] }))
+        app.use('/api/issues', require('./routes/issueRouter.js'))
+        
+        app.use((err, req, res, next) => {
+            console.log(err)
+            if(err.name === "UnauthorizedError"){
+                res.status(err.status)
+            }
+            return res.send({ errMsg: err.message })
+        })
+        
+        
+        app.get("*", (req, res) => {
+            res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+        });
+        
+        app.listen(port, () => {
     console.log(`Server is running on local port ${port}`)
 })
