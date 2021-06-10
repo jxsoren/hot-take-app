15488c804eb160bd8c01b96fc367924856fb6908
josh sorensen <jxsoren@gmail.com>
2021-06-09 21:02:58 -0600

    auth

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+require('dotenv').config({ path: 'env file path' })
 const dotenv = require('dotenv')
 dotenv.config()
 const morgan = require('morgan')
