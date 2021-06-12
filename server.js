d83479c904c7ad4e94ed6a706518a83ec643ce37
josh sorensen <jxsoren@gmail.com>
2021-06-12 00:23:03 -0600

    changes

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 const express = require('express')
 const app = express()
-require('dotenv').config()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const expressJwt = require('express-jwt')
-require('dotenv').config({ path: 'env file path' })
-
-app.use('/api', expressJwt({ secret: process.env.SECRET, algorithms:['HS256'] }))
-
-// const secret = process.env.SECRET || "strafe pandafff blinds cool man hame fool haha frame"
-
-app.use('/api/issues', require('./routes/issueRouter.js'))
+require("dotenv").config()
 
 const path = require("path")
+const secret = process.env.SECRET || "strafe pandafff blinds cool man hame fool haha frame"
+
+const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(morgan('dev'))
@@ -31,24 +27,23 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', MONGO_URI => console.log('Connected to Database'))
 
-const port = process.env.PORT || 5000;
+app.use('/api', expressJwt({ secret: process.env.SECRET, algorithms:['HS256'] }))
+app.use('/api/issues', require('./routes/issueRouter.js'))
+app.use('/auth', require('./routes/authRouter.js'))
         
-        app.use('/auth', require('./routes/authRouter.js'))
         
-        // gatekeeper 
-        
-        app.use((err, req, res, next) => {
-            console.log(err)
-            if(err.name === "UnauthorizedError"){
-                res.status(err.status)
-            }
-            return res.send({ errMsg: err.message })
-        })
-        
-        app.get("*", (req, res) => {
-            res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-        });
-        
-        app.listen(port, () => {
-    console.log(`Server is running on local port ${port}`)
+app.use((err, req, res, next) => {
+    console.log(err)
+    if(err.name === "UnauthorizedError"){
+        res.status(err.status)
+    }
+    return res.send({ errMsg: err.message })
+})
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+app.listen(port, () => {
+console.log(`Server is running on local port ${port}`)
 })
